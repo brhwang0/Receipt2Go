f5ee4d774f9a6bab67609d0b4bde62f36bf0a209
brhwang0 <brhwang@protonmail.com>
2020-07-03 16:19:46 -0400

    add json fetch, update list to display order information

--- app/src/main/java/com/example/receipt2go/MainActivity.java
+++ app/src/main/java/com/example/receipt2go/MainActivity.java
@@ -5,8 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,17 +28,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initOrders();
+        
+        fetchOrders("http://sushistamford.com/menuDB/OrdersController.php?view=all");
     }
 
-    private void initOrders() {
-        orders.add(new Order("Order #1", "12:00 PM", "John Doe"));
-        orders.add(new Order("Order #2", "12:15 PM", "Jane Doe"));
+    // Fetches open orders from URL
+    private void fetchOrders(final String urlWebService) {
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
 
-        initRecyclerView();
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String json) {
+                super.onPostExecute(json);
+                try {
+                    Log.d("tag", json);
+                    // JSON data fetched, now parse and initialize orders
+                    initOrders(json);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return e.getMessage();
+                }
+            }
+        }
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
     }
 
+    // Populate the array with current open orders
+    private void initOrders(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray jsonArray = jsonObject.getJSONArray("orders");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                orders.add(new Order(obj.getString("Order_Number"), obj.getString("Order_Date"), obj.getString("Order_Number")));
+            }
+            initRecyclerView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Creates a recycler view of open orders
     private void initRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.rvOrders);
         RVAdapter adapter = new RVAdapter(this, orders);
